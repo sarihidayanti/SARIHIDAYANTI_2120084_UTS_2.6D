76f8fed744c60596c264802c779161de333a2d92
Sari Hidayanti <165822121+sarihidayanti@users.noreply.github.com>
2024-05-20 14:58:21 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function tambah() {
+    var num1 = parseInt(document.getElementById('num1').value);
+    var num2 = parseInt(document.getElementById('num2').value);
+    var result = num1 + num2;
+    document.getElementById('result').innerText = "Hasil: " + result;
+}
